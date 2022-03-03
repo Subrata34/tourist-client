87af35ca2e5a607b88320686028205f02893ce41
Subrata34 <subratamodak3491@gmail.com>
2022-03-03 13:08:05 +0600

    files

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from '../Footer/Footer'
 import Menubar from '../Menubar/Menubar'
 import Topbanner from '../Topbanner/Topbanner'
 
 const Home = () => {
+  const[products,setProducts]=useState();
+  useEffect(()=>{
+    fetch('http://localhost:5000/place')
+    .then(res=>res.json())
+    .then(data=>setProducts(data));
+  },[]);
   return (
     <div>
       <Menubar></Menubar>
       <Topbanner></Topbanner>
-      <h1>this is home button </h1>
+      <div>
+      
+        
+      </div>
       <Footer></Footer>
     </div>
   )
