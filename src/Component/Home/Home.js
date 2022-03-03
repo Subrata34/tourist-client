24167c97caffa3bae5eac1f20d2f6ba3c966bc56
Subrata34 <subratamodak3491@gmail.com>
2022-03-03 19:33:14 +0600

    files

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,22 +1,18 @@
+import { Grid } from '@mui/material'
 import React, { useEffect, useState } from 'react'
+import Box from '../Box/Box'
 import Footer from '../Footer/Footer'
 import Menubar from '../Menubar/Menubar'
 import Topbanner from '../Topbanner/Topbanner'
 
 const Home = () => {
-  const[products,setProducts]=useState();
-  useEffect(()=>{
-    fetch('http://localhost:5000/place')
-    .then(res=>res.json())
-    .then(data=>setProducts(data));
-  },[]);
+ 
   return (
     <div>
       <Menubar></Menubar>
       <Topbanner></Topbanner>
       <div>
-      
-        
+     <h1>Home</h1>
       </div>
       <Footer></Footer>
     </div>
